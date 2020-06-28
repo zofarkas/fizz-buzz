6f383b974dc613df889335f86585b5a2fa6e3ac0
Zoltan <f.zoli05@gmail.com>
2020-06-28 20:05:12 +0200

    Add app skeleton

---
+++ src/main/java/com/example/fizzbuzz/FizzBuzzApplication.java
@@ -0,0 +1,13 @@
+package com.example.fizzbuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzBuzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizzBuzzApplication.class, args);
+	}
+
+}
